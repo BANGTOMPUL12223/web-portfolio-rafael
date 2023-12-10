91ebac4ab9749e08533e2acf70f20fc5cc66754b
rizfanradya <140232358+rizfanradya@users.noreply.github.com>
2023-12-10 03:04:28 +0000

    up

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -1,5 +1,23 @@
 import Navbar from "@/components/navbar";
+import Image from "next/image";
 
 export default function about() {
-  return <Navbar>about</Navbar>;
+  return (
+    <Navbar>
+      <div>
+        <div className="border border-slate-300/25 rounded-3xl bg-gray-900 p-5">
+          <p className="font-semibold text-2xl">About Me</p>
+          <div className="max-w-sm">
+            <Image
+              className="w-full"
+              src={"/default.png"}
+              alt="profile"
+              width={300}
+              height={300}
+            />
+          </div>
+        </div>
+      </div>
+    </Navbar>
+  );
 }
